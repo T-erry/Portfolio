398f1eec949d2127f337f32d9edcd7e6f2f5bdee
T-erry <terrywachuka10@gmail.com>
2023-05-17 02:23:05 +0300

    initial commit

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+import WorkIm from '../assets/projects/workImg.jpeg';
+import RealEst from '../assets/projects/realestate.jpg';
+import Article from '../assets/projects/article.png';
+import Gym from '../assets/projects/Gym.png';
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Article,
+        github:"https://github.com/T-erry/article-management-?organization=T-erry&organization=T-erry"
+       
+    },
+    {
+        id:2,
+        name:"React JS and Rails",
+        image:Gym,
+        github:"https://github.com/T-erry/gym-finder"
+       
+    }
+    
+]
