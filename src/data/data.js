32069bc44b93e2f9245b48e800b444bfec3d7444
T-erry <terrywachuka10@gmail.com>
2023-05-17 15:52:11 +0300

    refactored the components

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,7 @@ import WorkIm from '../assets/projects/workImg.jpeg';
 import RealEst from '../assets/projects/realestate.jpg';
 import Article from '../assets/projects/article.png';
 import Gym from '../assets/projects/Gym.png';
+import Dictionary from '../assets/projects/Dictionary.png';
 
 export const data=[
     {
@@ -12,11 +13,20 @@ export const data=[
        
     },
     {
+        id:3,
+        name:"React JS Application",
+        image:Dictionary,
+        github:"https://github.com/T-erry/dictionary-app"
+    
+    },
+    {
         id:2,
-        name:"React JS and Rails",
+        name:"A fullstack application using React and Ruby on Rails",
         image:Gym,
         github:"https://github.com/T-erry/gym-finder"
        
     }
+    
+    
     
 ]
