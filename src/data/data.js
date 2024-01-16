0c88542dbffd5a434c2f87ab4a4ffc2ed1427070
T-erry <terrywachuka10@gmail.com>
2024-01-16 16:48:23 +0300

    updated data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,24 @@
-import WorkIm from '../assets/projects/workImg.jpeg';
-import RealEst from '../assets/projects/realestate.jpg';
-import Article from '../assets/projects/article.png';
+
+import house from '../assets/house.png';
 import Gym from '../assets/projects/Gym.png';
+
 import Dictionary from '../assets/projects/Dictionary.png';
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:Article,
-        github:"https://github.com/T-erry/article-management-?organization=T-erry&organization=T-erry"
+        name:" Real-estate-marketplace",
+        image: house,
+        github:"https://github.com/T-erry/real-estate-application",
+        live: "https://real-estate-marketplace-three.vercel.app/"
        
     },
     {
         id:3,
         name:"React JS Application",
         image:Dictionary,
-        github:"https://github.com/T-erry/dictionary-app"
+        github:"https://github.com/T-erry/dictionary-app",
+        live: "https://search-words.netlify.app/"
     
     },
     {
